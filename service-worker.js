bd75b008bad430016516e6907a71cb5dc8b53b2a
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2025-12-14 22:29:23 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,23 @@
+// service-worker.js
 const CACHE_NAME = 'meu-espaco-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
 ];
 
-// Instalando Service Worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', event=>{
+    event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
 
-// Ativando Service Worker
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if(key !== CACHE_NAME) return caches.delete(key);
-      })
-    ))
-  );
+self.addEventListener('activate', event=>{
+    event.waitUntil(caches.keys().then(keys=>Promise.all(
+        keys.map(key=>{if(key!==CACHE_NAME) return caches.delete(key);})
+    )));
 });
 
-// Fazendo fetch do cache
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', event=>{
+    event.respondWith(caches.match(event.request).then(response=>response || fetch(event.request)));
 });
